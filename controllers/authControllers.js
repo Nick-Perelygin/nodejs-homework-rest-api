5ae950461fcf5f54d7139e121cbcde35ae74d858
Nick-Perelygin <114926824+Nick-Perelygin@users.noreply.github.com>
2023-07-31 23:37:46 +0300

    auth2

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,18 +1,18 @@
 const bcrypt = require('bcrypt')
 const {userSchema} = require('../schemas');
-const {User} = userSchema
+const users = userSchema.User
 const {HttpError, ctrlWrapper} = require('../utility');
 const jwt = require('jsonwebtoken')
 const {SECRET_KEY} = process.env;
 
 async function register (req, res) {
     const {email, password} = req.body;
-    const user = await User.findOne({email});
+    const user = await users.findOne({email});
     if(user){
         throw HttpError(409, 'Email already in use')
     }
     const hashPassword = await bcrypt.hash(password, 10)
-    const newUser = await User.create(...req.body, password, hashPassword);
+    const newUser = await users.create(...req.body, password, hashPassword);
     res.status(201).json({
         email: newUser.email,
         name: newUser.name
@@ -21,7 +21,7 @@ async function register (req, res) {
 
 async function login (req, res) {
     const {email, password} = req.body;
-    const user = await User.findOne({email});
+    const user = await users.findOne({email});
     if(user){
         throw HttpError(401, 'Email or password invalid')
     }
@@ -33,7 +33,7 @@ async function login (req, res) {
         id: user._id,
     }
     const token = jwt.sign(payload, SECRET_KEY, {expiresIn: '24h'})
-    await User.findByIdAndUpdate(user._id, {token})
+    await users.findByIdAndUpdate(user._id, {token})
     res.status(201).json({
         token
     });
@@ -49,7 +49,7 @@ async function current (req, res) {
 
 async function logout (req, res) {
     const {_id} = req.user;
-    await User.findByIdAndUpdate(_id, {token: ''})
+    await users.findByIdAndUpdate(_id, {token: ''})
 
     res.json({
         message: 'Logout success'
