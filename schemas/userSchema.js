6d92003f6f762a8f418a3b9a2d6c22bbec0957cf
Nick-Perelygin <114926824+Nick-Perelygin@users.noreply.github.com>
2023-08-02 00:11:59 +0300

    auth13

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -36,7 +36,7 @@ const registerSchema = Joi.object({
 })
 
 const loginSchema = Joi.object({
-    name: String,
+    name: Joi.string(),
     email: Joi.string()
     .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } }),
     password: Joi.number().min(6).required(),
