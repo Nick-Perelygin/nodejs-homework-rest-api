0a80f6d86232bae505669a76953feb1cf484ba5b
Nick-Perelygin <114926824+Nick-Perelygin@users.noreply.github.com>
2023-08-02 00:20:27 +0300

    auth12

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -11,7 +11,7 @@ async function register (req, res) {
     if(user){
         throw HttpError(409, 'Email already in use')
     }
-    const hashPassword = await bcrypt.hash(password, 10)
+    const hashPassword = bcrypt.hash(password, 10)
     const newUser = await User.create({...req.body, password, hashPassword});
     res.status(201).json({
         email: newUser.email,
