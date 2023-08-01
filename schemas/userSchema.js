8ab6c63ecaae0a804c7d3841a9ce63873a2a3b0b
Nick-Perelygin <114926824+Nick-Perelygin@users.noreply.github.com>
2023-08-01 20:40:43 +0300

    auth5

--- schemas/userSchema.js
+++ schemas/userSchema.js
@@ -3,6 +3,7 @@ const {Schema, model} = require('mongoose')
 const {handleMongooseError} = require('../utility')
 
 const userSchema = new Schema({
+    name: String,
     password: {
       type: String,
       required: [true, 'Set password for user'],
